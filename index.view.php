358d71669c0e89e002af9f7816a23ffa0e951d7a
Bharat Narendranath <bharatnarendranath@gmail.com>
2018-09-10 20:16:14 +0530

    associate endpoint with a controller

---
+++ index.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP Excercises</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($tasks as $task) : ?>
+
+        <li>
+
+            <?php if($task->completed) :?>
+
+            <strike>
+                <?= $task->description; ?>
+            </strike>
+
+            <?php else: ?>
+
+            <?= $task->description; ?>
+
+            <?php endif; ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+
+</html>
---
+++ test1.php
@@ -0,0 +1,46 @@
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Blah blah</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <style>
+        header {
+            background: #0080c0;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <ul>
+        <?php foreach($animals as $animal): ?>
+        <li>
+            <?=$animal; ?>
+        </li>
+
+        <?php endforeach ; ?>
+
+        <?php 
+    foreach($animals as $animal){
+        echo "<li>$animal</li>";
+    }
+        ?>
+
+        <?php foreach ($task as $key => $items): ?>
+        <li>
+            <strong>
+                <?= ucwords($key); ?>:
+            </strong>
+            <?= $items; ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html> -->
